332bd60b04536ad4631cf19e1a68c72c7348947c
ken <156801554@qq.com>
2020-01-26 21:43:29 +0800

    menu

--- src/main/java/jken/site/endpoint/IndexEndpoint.java
+++ src/main/java/jken/site/endpoint/IndexEndpoint.java
@@ -1,12 +1,21 @@
 package jken.site.endpoint;
 
+import jken.site.modules.core.service.MenuItemService;
+import jken.site.support.data.TreeHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexEndpoint {
+
+    @Autowired
+    private MenuItemService menuItemService;
+
     @GetMapping({"", "/"})
-    public String indexPage() {
+    public String indexPage(Model model) {
+        model.addAttribute("mis", TreeHelper.toTree(menuItemService.findAll()));
         return "/index";
     }
 
